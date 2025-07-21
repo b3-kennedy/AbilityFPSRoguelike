a29d5fae41b6a0f265bb96312074417df2c5d307
b3-kennedy <Benjamin3.Kennedy@live.uwe.ac.uk>
2025-07-21 12:28:18 +0100

    added audio queues for flying enemy attacks

--- Assets/FlyingEnemyMove.cs
+++ Assets/FlyingEnemyMove.cs
@@ -1,8 +1,9 @@
+using Unity.Netcode;
+using Unity.Netcode.Components;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngineInternal;
 
-public class FlyingEnemyMove : MonoBehaviour
+
+public class FlyingEnemyMove : NetworkBehaviour
 {
     Rigidbody rb;
     Vector3 directionToTarget;
@@ -12,6 +13,17 @@ public class FlyingEnemyMove : MonoBehaviour
     public float attackRange;
     TargetHolder holder;
 
+    [Header("Attack Settings")]
+    public float attackCooldown;
+    public float attackChargeTime;
+    float attackChargeTimer;
+    public float damage;
+    bool isAttacking;
+    public AudioSource attackAudioSource;
+    public AudioSource chargeAudioSource;
+
+    float attackCooldownTimer;
+    bool isOnCooldown;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -26,9 +38,44 @@ public class FlyingEnemyMove : MonoBehaviour
     {
         if (!holder.target) return;
 
+        if (isOnCooldown)
+        {
+            attackCooldownTimer += Time.deltaTime;
+            if(attackCooldownTimer >= attackCooldown)
+            {
+                isOnCooldown = false;
+                attackCooldownTimer = 0;
+            }
+
+        }
+
+
         if (Vector3.Distance(transform.position, holder.target.position) <= attackRange)
         {
             rb.linearVelocity = Vector3.zero;
+
+            if (!isOnCooldown)
+            {
+                if (!isAttacking)
+                {
+                    chargeAudioSource.PlayOneShot(chargeAudioSource.clip);
+                }
+                isAttacking = true;
+
+
+                if (isAttacking)
+                {
+                    attackChargeTimer += Time.deltaTime;
+                    if (attackChargeTimer >= attackChargeTime)
+                    {
+                        Attack();
+                        attackChargeTimer = 0;
+                    }
+                }
+            }
+
+
+
         }
 
         timer += Time.deltaTime;
@@ -48,12 +95,26 @@ public class FlyingEnemyMove : MonoBehaviour
                 Vector3? teleportPos = FindTeleportPosition();
                 if (teleportPos.HasValue)
                 {
+                    GetComponent<NetworkTransform>().Teleport(teleportPos.Value, transform.rotation, transform.localScale);
                     transform.position = teleportPos.Value;
                 }
             }
             timer = 0;
         }
 
+    }
+
+    void Attack()
+    {
+        attackAudioSource.PlayOneShot(attackAudioSource.clip);
+        Health health = holder.target.GetComponent<Health>();
+        if (health)
+        {
+            holder.target.GetComponent<Health>().TakeDamageServerRpc(damage);
+        }
+        
+        isAttacking = false;
+        isOnCooldown = true;
     }
 
     bool CanSeeTarget()
@@ -72,7 +133,7 @@ public class FlyingEnemyMove : MonoBehaviour
 
     Vector3? FindTeleportPosition()
     {
-        float radius = 20f; // How far from the target to search
+        float radius = 50f; // How far from the target to search
         int checks = 10; // Number of positions to try
         float heightOffset = 10f; // How high off the ground the enemy can teleport
 
@@ -80,7 +141,7 @@ public class FlyingEnemyMove : MonoBehaviour
         {
             // Random direction around target
             Vector3 randomDirection = Random.onUnitSphere;
-            randomDirection.y = Mathf.Clamp(randomDirection.y, 0.2f, 1f); // Keep above ground-ish
+            randomDirection.y = Mathf.Clamp(randomDirection.y, 5f, 50f); // Keep above ground-ish
             Vector3 candidatePos = holder.target.position + randomDirection * radius;
             Debug.Log(candidatePos);
             // Check visibility from candidatePos to target
